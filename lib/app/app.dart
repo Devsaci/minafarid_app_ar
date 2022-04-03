b9dc4c313532352f342c6fbbaa9d5c3ada4f03e5
saci zakaria <sacideveloppeur@gmail.com>
2022-04-03 13:24:25 +0200

    class test  / method updateAppState

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
+  int appState = 0;
+
   // named constructor
   MyApp.internal();
 
@@ -13,6 +15,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+//class test
+class test extends StatelessWidget {
+  const test({Key? key}) : super(key: key);
+
+  //method updateAppState
+  void updateAppState() {
+    MyApp().appState = 11;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
