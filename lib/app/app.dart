6734f808b36e6fcfdf48885b8ab0dc5faf436aa7
saci zakaria <sacideveloppeur@gmail.com>
2022-04-03 12:47:38 +0200

    named constructor

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,13 +3,22 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key); // Default Constructor
+  // const MyApp({Key? key}) : super(key: key); // Default Constructor
+
+
+  // named constructor
+   MyApp.internal();
+
 
   @override
   State<MyApp> createState() => _MyAppState();
+
+
 }
 
 class _MyAppState extends State<MyApp> {
+
+
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'app/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp.internal());
 
-  MyApp app1 = const MyApp();
-  MyApp app2 = const MyApp();
-  MyApp app3 = const MyApp();
+  // MyApp app1 = const MyApp();
+  // MyApp app2 = const MyApp();
+  // MyApp app3 = const MyApp();
 }
