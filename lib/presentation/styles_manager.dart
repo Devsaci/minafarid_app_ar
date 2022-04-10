63f5f7ada7a4f753169fce603ce74a8155c28fd1
saci zakaria <sacideveloppeur@gmail.com>
2022-04-10 19:14:58 +0200

    fontWeight: fontWeight,

--- lib/presentation/styles_manager.dart
+++ lib/presentation/styles_manager.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 TextStyle _getTextStyle(
   double fontSize,
+  FontWeight fontWeight,
 ) {
   return TextStyle(
     fontSize: fontSize,
+    fontWeight: fontWeight,
   );
 }
