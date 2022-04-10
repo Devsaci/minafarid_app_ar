8474922ba0a94a466404898e4f122e2806a7be5b
saci zakaria <sacideveloppeur@gmail.com>
2022-04-10 19:19:14 +0200

    fontFamily: FontConstants.fontFamily,

--- lib/presentation/styles_manager.dart
+++ lib/presentation/styles_manager.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'font_manager.dart';
+
 TextStyle _getTextStyle(
   double fontSize,
   FontWeight fontWeight,
   Color color,
+  FontConstants fontFamily,
 ) {
   return TextStyle(
     fontSize: fontSize,
     fontWeight: fontWeight,
     color: color,
+    fontFamily: FontConstants.fontFamily,
   );
 }
