df42171887adc57b188f7d1c0308b4588577ba86
saci zakaria <sacideveloppeur@gmail.com>
2022-04-11 23:39:46 +0200

    List ToDo

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
-  return ThemeData();
+  return ThemeData(
+    // main colors
+
+    // cardview theme
+
+    // app bar theme
+
+    // button theme
+
+    // text theme
+
+    // input decoration theme (text form field)
+  );
 }
