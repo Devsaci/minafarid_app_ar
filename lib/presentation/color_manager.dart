ff8305bea63ab7d541a417e040022e4f16261175
saci zakaria <sacideveloppeur@gmail.com>
2022-04-11 23:44:43 +0200

    grey2

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -10,4 +10,5 @@ class ColorManager {
   static Color darkPrimary = const Color(0xffd17d11);
   static Color lightPrimary = const Color(0xCCd17d11); // color with 80% opacity
   static Color grey1 = const Color(0xff707070);
+  static Color grey2 = const Color(0xff797979);
 }
