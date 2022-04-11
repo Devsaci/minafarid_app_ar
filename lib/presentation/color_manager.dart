9ce82498f3e1dbe3f3a84eef755bc4d9b76debd5
saci zakaria <sacideveloppeur@gmail.com>
2022-04-11 23:42:15 +0200

    darkPrimary

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -5,4 +5,7 @@ class ColorManager {
   static Color darkGrey = const Color(0xff525252);
   static Color grey = const Color(0xff737477);
   static Color lightGrey = const Color(0xff9E9E9E);
+
+  // new colors
+  static Color darkPrimary = const Color(0xffd17d11);
 }
