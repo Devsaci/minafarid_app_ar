6145d97d482f7f1c79bc849c78e0d41fc752f3ad
saci zakaria <sacideveloppeur@gmail.com>
2022-05-04 09:29:23 +0200

    class Routes / "forgotPassword"

--- lib/presentation/routes_manager.dart
+++ lib/presentation/routes_manager.dart
@@ -3,6 +3,7 @@ class Routes {
   static const String splashRoute = "/";
   static const String loginRoute = "/login";
   static const String registerRoute = "/register";
+  static const String forgotPasswordRoute = "/forgotPassword";
 
 
 
