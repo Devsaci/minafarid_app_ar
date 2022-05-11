1ba90427c3504d2e0dfa86218e7af1dd9be2c2ae
saci zakaria <sacideveloppeur@gmail.com>
2022-05-11 11:11:56 +0200

    _startDelay() {}

--- lib/presentation/splash/splash_view.dart
+++ lib/presentation/splash/splash_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:minafarid_app_ar/presentation/resources/assets_manager.dart';
 import 'package:minafarid_app_ar/presentation/resources/color_manager.dart';
@@ -13,12 +15,14 @@ class _SplashViewState extends State<SplashView> {
   //21. Lecture 21 - Adding Timer to Splash Screen
   Timer? _timer;
 
+  _startDelay() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.primary,
       body:
-      const Center(child: Image(image: AssetImage(ImageAssets.splashLogo))),
+          const Center(child: Image(image: AssetImage(ImageAssets.splashLogo))),
     );
   }
 }
