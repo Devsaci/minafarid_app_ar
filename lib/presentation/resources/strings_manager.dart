8f23a6c96e34d5dcf512ba526c818a68bbf0e878
saci zakaria <sacideveloppeur@gmail.com>
2022-05-12 21:20:54 +0200

    "SEE THE BEST COURSE #1"

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,4 +1,5 @@
 class AppStrings {
   static const noRouteFound = "No Route Found";
   //22. Lecture 22 - Adding Assets for Onboarding Screen
+  static const onBoardingTitle1 = "SEE THE BEST COURSE #1";
 }
