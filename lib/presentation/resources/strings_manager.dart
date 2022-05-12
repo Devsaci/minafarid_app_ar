0ef76258d78bbdc5ed49addde7b78b3b7234f774
saci zakaria <sacideveloppeur@gmail.com>
2022-05-12 21:23:24 +0200

    "Tut app is an awesome flutter application using clean architecture #1";

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -5,4 +5,8 @@ class AppStrings {
   static const onBoardingTitle2 = "SEE THE BEST COURSE #2";
   static const onBoardingTitle3 = "SEE THE BEST COURSE #3";
   static const onBoardingTitle4 = "SEE THE BEST COURSE #4";
+
+
+  static const onBoardingSubTitle1 =
+      "Tut app is an awesome flutter application using clean architecture #1";
 }
