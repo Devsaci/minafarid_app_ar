2c70fa305ba58e10066c4737434c12b2c2388655
saci zakaria <sacideveloppeur@gmail.com>
2022-05-12 22:00:45 +0200

    onboardingLogo2 = "$imagePath/onboarding_logo2.svg"

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -3,5 +3,6 @@ const String imagePath = "assets/images";
 class ImageAssets {
   static const String splashLogo = "$imagePath/splash_logo.png";
   static const String onboardingLogo1 = "$imagePath/onboarding_logo1.svg";
+  static const String onboardingLogo2 = "$imagePath/onboarding_logo2.svg";
 }
 
