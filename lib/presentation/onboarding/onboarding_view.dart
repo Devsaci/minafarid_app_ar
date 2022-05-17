55f531a5b9dccd1cead7dba374f10a13bb2b0f63
saci zakaria <sacideveloppeur@gmail.com>
2022-05-17 16:22:03 +0200

    Column

--- lib/presentation/onboarding/onboarding_view.dart
+++ lib/presentation/onboarding/onboarding_view.dart
@@ -73,7 +73,10 @@ class OnBoardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+
+    );
   }
 }
 
