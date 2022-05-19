e02a39d627db5db8c1ce08c8a3c1eb0063a2f0d5
saci zakaria <sacideveloppeur@gmail.com>
2022-05-19 10:14:04 +0200

    hollow_cirlce_ic

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -6,5 +6,7 @@ class ImageAssets {
   static const String onboardingLogo2 = "$imagePath/onboarding_logo2.svg";
   static const String onboardingLogo3 = "$imagePath/onboarding_logo3.svg";
   static const String onboardingLogo4 = "$imagePath/onboarding_logo4.svg";
+  static const String hollow_cirlce_ic = "$imagePath/hollow_cirlce_ic.svg";
+
 }
 
