e7388f6904295fcca3a96d7e8943c9baac15fe7b
saci zakaria <sacideveloppeur@gmail.com>
2022-05-22 20:49:56 +0200

    extends BaseViewModel

--- lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
+++ lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
@@ -1,2 +1,16 @@
 //32. Lecture 32 - Creating Onboarding Screen ViewModel
-class OnBoardingViewModel{}
+import 'package:minafarid_app_ar/presentation/base/baseviewmodel.dart';
+
+class OnBoardingViewModel extends BaseViewModel{
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+
+}
