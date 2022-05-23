bc10950bea9e1a9c529f78f5b62859b8fdcded99
saci zakaria <sacideveloppeur@gmail.com>
2022-05-23 12:08:37 +0200

    _list = _getSliderData() /

--- lib/presentation/onboarding/viewmodel/onboarding_view_model.dart
+++ lib/presentation/onboarding/viewmodel/onboarding_view_model.dart
@@ -4,21 +4,25 @@ import 'dart:async';
 import 'package:minafarid_app_ar/presentation/base/base_view_model.dart';
 
 import '../../../domain/models.dart';
+import '../../resources/assets_manager.dart';
+import '../../resources/strings_manager.dart';
 
 class OnBoardingViewModel extends BaseViewModel
     with OnBoardingViewModelInputs, OnBoardingViewModelOutputs {
   // stream controllers outputs
   StreamController _streamController = StreamController<SliderViewObject>();
+  late final List<SliderObject> _list;
 
   //OnBoarding ViewModel Inputs
   @override
   void dispose() {
-  _streamController.close();
+    _streamController.close();
   }
 
   @override
   void start() {
-    // TODO: implement start
+    // view model start your job
+    _list = _getSliderData();
   }
 
   @override
@@ -45,6 +49,30 @@ class OnBoardingViewModel extends BaseViewModel
   // TODO: implement outputSliderViewObject
   Stream get outputSliderViewObject => throw UnimplementedError();
 }
+
+// onboarding private functions
+List<SliderObject> _getSliderData() => [
+      SliderObject(
+        AppStrings.onBoardingTitle1,
+        AppStrings.onBoardingSubTitle1,
+        ImageAssets.onboardingLogo1,
+      ),
+      SliderObject(
+        AppStrings.onBoardingTitle2,
+        AppStrings.onBoardingSubTitle2,
+        ImageAssets.onboardingLogo2,
+      ),
+      SliderObject(
+        AppStrings.onBoardingTitle3,
+        AppStrings.onBoardingSubTitle3,
+        ImageAssets.onboardingLogo3,
+      ),
+      SliderObject(
+        AppStrings.onBoardingTitle4,
+        AppStrings.onBoardingSubTitle4,
+        ImageAssets.onboardingLogo4,
+      ),
+    ];
 
 // inputs mean that "Orders" that our view model will receive from view
 abstract class OnBoardingViewModelInputs {
