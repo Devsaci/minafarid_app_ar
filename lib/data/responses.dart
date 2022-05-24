fa0be23d7508aed3f798e3187403522bff34ea3e
saci zakaria <sacideveloppeur@gmail.com>
2022-05-24 21:25:31 +0200

    @JsonSerializable()

--- lib/data/responses.dart
+++ lib/data/responses.dart
@@ -1,5 +1,7 @@
 //  43. Lecture 43 - Data Layer - Adding APIs Responses
+import 'package:json_annotation/json_annotation.dart';
 
+@JsonSerializable()
 class BaseResponse {
   int? status;
   String? message;
