db533580670e23fcd400d1df2b839e949770077b
saci zakaria <sacideveloppeur@gmail.com>
2022-05-24 21:27:21 +0200

    @JsonKey(name: "message")

--- lib/data/responses.dart
+++ lib/data/responses.dart
@@ -3,6 +3,8 @@ import 'package:json_annotation/json_annotation.dart';
 
 @JsonSerializable()
 class BaseResponse {
+  @JsonKey(name: "status")
   int? status;
+  @JsonKey(name: "message")
   String? message;
 }
