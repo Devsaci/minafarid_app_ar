8c7b4e8c169509c83f6bb7296ff0735646f3949e
saci zakaria <sacideveloppeur@gmail.com>
2022-05-25 11:21:21 +0200

    factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;

--- lib/data/app_api.dart
+++ lib/data/app_api.dart
@@ -1,10 +1,10 @@
 //  45. Lecture 45 - Data Layer - Add API Service Client (Retrofit)
 
 import 'package:retrofit/http.dart';
-
+import 'package:dio/dio.dart';
 import '../app/constants.dart';
 
 @RestApi(baseUrl: Constants.baseUrl)
 abstract class AppServiceClient{
-
+factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient; // Error
 }
