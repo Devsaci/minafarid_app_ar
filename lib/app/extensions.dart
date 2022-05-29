9adf6f9c25eda5e4e61ea006d28e27e829166ef4
saci zakaria <sacideveloppeur@gmail.com>
2022-05-29 08:01:05 +0200

    String orEmpty()

--- lib/app/extensions.dart
+++ lib/app/extensions.dart
@@ -1,3 +1,7 @@
 //  47. Lecture 47 - Application Layer - Creating Extensions Functions
 
-extension NonNullString on String{}
+extension NonNullString on String {
+  String orEmpty() {
+    return "string";
+  }
+}
