cb6e2c8f406407d15f73942ed313135f9d71de2f
saci zakaria <sacideveloppeur@gmail.com>
2022-05-30 21:40:55 +0200

    this?.id.orEmpty() ?? ""

--- lib/data/mapper/mapper.dart
+++ lib/data/mapper/mapper.dart
@@ -1,11 +1,15 @@
 // 48. Lecture 48 - Data Layer - Creating Mappers and Applying Null Safety
 
 import 'package:minafarid_app_ar/data/response/responses.dart';
-
 import '../../domain/models.dart';
+import 'package:minafarid_app_ar/app/extensions.dart';
 
 extension CustomerResponseMapper on CustomerResponse? {
   Customer toDomain() {
-    return Customer(this?.id ?? "",  this?.name ?? "",  this?.numOfNotifications ?? 0 );
+    return Customer(
+      this?.id.orEmpty() ?? "",
+      this?.name ?? "",
+      this?.numOfNotifications ?? 0,
+    );
   }
 }
