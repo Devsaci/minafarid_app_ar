fe5fc4a6757503b22414e711795fffdad51d436b
saci zakaria <sacideveloppeur@gmail.com>
2022-06-02 19:14:03 +0200

    class Failure {}

--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -1,7 +1,9 @@
 // 50. Lecture 50 - Domain Layer - Adding Repository, Request and Failure
 
+import 'package:dartz/dartz.dart';
 import 'package:minafarid_app_ar/data/network/requests.dart';
+import 'package:minafarid_app_ar/data/response/responses.dart';
 
-abstract class Repository{
+abstract class Repository {
   login(LoginRequest loginRequest);
 }
