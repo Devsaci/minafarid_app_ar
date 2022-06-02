292081d9d322f2bd02ddcbe7e5b4823bee9a67e9
saci zakaria <sacideveloppeur@gmail.com>
2022-06-02 19:03:51 +0200

    login(LoginRequest loginRequest);

--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -1,5 +1,7 @@
 // 50. Lecture 50 - Domain Layer - Adding Repository, Request and Failure
 
+import 'package:minafarid_app_ar/data/network/requests.dart';
+
 abstract class Repository{
-  login();
+  login(LoginRequest loginRequest);
 }
