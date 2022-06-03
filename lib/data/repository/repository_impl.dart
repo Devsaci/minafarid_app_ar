5012e27282b301609d83213c5ca032b217ff28d7
saci zakaria <sacideveloppeur@gmail.com>
2022-06-03 21:54:48 +0200

    if (await _networkInfo.isConnected)

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,5 +1,6 @@
 // 53. Lecture 53 - Data Layer - Adding Repository Implementer
 import 'package:dartz/dartz.dart';
+import 'package:minafarid_app_ar/data/mapper/mapper.dart';
 
 import 'package:minafarid_app_ar/data/network/failure.dart';
 
@@ -12,14 +13,18 @@ import '../data_source/remote_data_source.dart';
 import '../network/network_info.dart';
 
 class RepositoryImpl implements Repository {
-   final RemoteDataSource _remoteDataSource;
-   final NetworkInfo _networkInfo;
+  final RemoteDataSource _remoteDataSource;
+  final NetworkInfo _networkInfo;
 
   RepositoryImpl(this._remoteDataSource, this._networkInfo);
 
   @override
-  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<Either<Failure, Authentication>> login(// Error return
+      LoginRequest loginRequest) async {
+    if (await _networkInfo.isConnected) {
+      // its connected to internet, its safe to call API
+    }else{
+      // return internet connection error
+    }
   }
 }
