309c829fb987598651d0a0a14696b1c4c576d1e2
saci zakaria <sacideveloppeur@gmail.com>
2022-06-03 17:53:55 +0200

    final AppServiceClient _appServiceClient;

--- lib/data/data_source/remote_data_source.dart
+++ lib/data/data_source/remote_data_source.dart
@@ -1,15 +1,20 @@
 //  51. Lecture 51 - Data Layer - Remote Data Source
 
+import 'package:minafarid_app_ar/data/network/app_api.dart';
 import 'package:minafarid_app_ar/data/network/requests.dart';
 import 'package:minafarid_app_ar/data/response/responses.dart';
 
 abstract class RemoteDataSource {
-  Future<AuthenticationResponse>  login(LoginRequest loginRequest);
+  Future<AuthenticationResponse> login(LoginRequest loginRequest);
 }
 
-class RemoteDataSourceImpl implements RemoteDataSource{
+class RemoteDataSourceImpl implements RemoteDataSource {
+  final AppServiceClient _appServiceClient;
+
+  RemoteDataSourceImpl(this._appServiceClient);
+
   @override
-  Future<AuthenticationResponse> login(LoginRequest loginRequest) async{
+  Future<AuthenticationResponse> login(LoginRequest loginRequest) async {
     // TODO: implement login
     throw UnimplementedError();
   }
