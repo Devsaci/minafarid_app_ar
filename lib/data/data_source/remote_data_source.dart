e6fe200b3caec2f5fc8b1dbcc3e36010fa9207f5
saci zakaria <sacideveloppeur@gmail.com>
2022-06-03 17:40:34 +0200

    login();

--- lib/data/data_source/remote_data_source.dart
+++ lib/data/data_source/remote_data_source.dart
@@ -1,5 +1,7 @@
 //  51. Lecture 51 - Data Layer - Remote Data Source
 
+import 'package:minafarid_app_ar/data/network/requests.dart';
+
 abstract class RemoteDataSource {
-  login();
+  login(LoginRequest loginRequest);
 }
