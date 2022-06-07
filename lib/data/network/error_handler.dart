aab4e0195ad4a3a03b7f4ae4bf3cad9759900db7
saci zakaria <sacideveloppeur@gmail.com>
2022-06-07 22:38:30 +0200

    NO_INTERNET_CONNECTION = -6;

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -29,5 +29,6 @@ class ResponseCode{
   static const int RECIEVE_TIMEOUT = -3; //
   static const int SEND_TIMEOUT = -4; //
   static const int CACHE_ERROR = -5; //
+  static const int NO_INTERNET_CONNECTION = -6; //
 
 }
