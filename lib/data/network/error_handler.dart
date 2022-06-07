99a0795b00b4a94debb4fd8422facf8cd3e6a992
saci zakaria <sacideveloppeur@gmail.com>
2022-06-07 22:39:19 +0200

    UNKNOWN = -7;

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -30,5 +30,6 @@ class ResponseCode{
   static const int SEND_TIMEOUT = -4; //
   static const int CACHE_ERROR = -5; //
   static const int NO_INTERNET_CONNECTION = -6; //
+  static const int UNKNOWN = -7; //
 
 }
