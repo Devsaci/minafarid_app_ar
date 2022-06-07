e02d3fab322ea622e7dcbdf2abb8aa7ab351d81b
saci zakaria <sacideveloppeur@gmail.com>
2022-06-07 22:33:08 +0200

    UNAUTORISED = 401;

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -19,4 +19,5 @@ class ResponseCode{
   static const int SUCCESS = 200; // success with data
   static const int NO_CONTENT = 201; // success with no data (no content)
   static const int BAD_REQUEST = 400; // failure, API rejected request
+  static const int UNAUTORISED = 401; // failure, user is not authorised
 }
