a6b23e3baa784db870ee70de69b198c99d08271d
saci zakaria <sacideveloppeur@gmail.com>
2022-06-28 21:13:47 +0200

    const String AUTHORIZATION = "authorization";

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 const String APPLICATION_JSON = "application/json";
 const String CONTENT_TYPE = "content-type";
 const String ACCEPT = "accept";
+const String AUTHORIZATION = "authorization";
 
 
 class DioFactory {
