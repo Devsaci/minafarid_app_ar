2ac2372702f8bc8e636edda28a5cb77d83934a46
saci zakaria <sacideveloppeur@gmail.com>
2022-06-28 21:14:11 +0200

    const String DEFAULT_LANGUAGE = "langauge";

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -6,6 +6,7 @@ const String APPLICATION_JSON = "application/json";
 const String CONTENT_TYPE = "content-type";
 const String ACCEPT = "accept";
 const String AUTHORIZATION = "authorization";
+const String DEFAULT_LANGUAGE = "langauge";const String DEFAULT_LANGUAGE = "langauge";
 
 
 class DioFactory {
