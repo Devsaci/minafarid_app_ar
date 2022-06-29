5d6c7c6832ae48ccb7f19c8d67d30eb70dadec59
saci zakaria <sacideveloppeur@gmail.com>
2022-06-29 18:41:42 +0200

    Lecture 62 Presentation Layer Adding Language Manager #53

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -4,4 +4,5 @@ class Constants {
   static const String baseUrl = "https://gldj.mocklab.io/";
   static const String empty = "";
   static const int zero = 0;
+  // Lecture 62 Presentation Layer Adding Language Manager #53
 }
