357fc08e8a4ee806a200fc3a43a919f23ea5a9cb
saci zakaria <sacideveloppeur@gmail.com>
2022-09-07 17:36:06 +0200

    class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication>

--- lib/domain/usecase/login_usecase.dart
+++ lib/domain/usecase/login_usecase.dart
@@ -1,1 +1,17 @@
 //Lecture 66 Domain Layer Adding Login UseCase #57
+import 'package:dartz/dartz.dart';
+
+import 'package:minafarid_app_ar/data/network/failure.dart';
+
+import '../model/models.dart';
+import 'base_usecase.dart';
+
+class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
+  @override
+  Future<Either<Failure, Authentication>>? execute(LoginUseCaseInput input) {
+    // TODO: implement execute
+    throw UnimplementedError();
+  }
+}
+
+class LoginUseCaseInput {}
