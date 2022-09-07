57d3227f1654dcc8c3b974ded50dbedfc598d701
saci zakaria <sacideveloppeur@gmail.com>
2022-09-07 17:15:34 +0200

    Future<Either<Failure, Out>>? execute(In input);

--- lib/domain/usecase/base_usecase.dart
+++ lib/domain/usecase/base_usecase.dart
@@ -1,3 +1,8 @@
 //Lecture 65 Domain Layer Adding Base UseCase #56
 
-abstract class BaseUseCase<In, Out>{}
+import 'package:dartz/dartz.dart';
+import 'package:minafarid_app_ar/data/network/failure.dart';
+
+abstract class BaseUseCase<In, Out> {
+  Future<Either<Failure, Out>>? execute(In input);
+}
