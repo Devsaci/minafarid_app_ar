e75eb67ea8cd86d9a6721cde51c6ff5f6abfb8f6
saci zakaria <sacideveloppeur@gmail.com>
2022-09-07 17:44:08 +0200

    LoginUseCase(this._repository);

--- lib/domain/usecase/login_usecase.dart
+++ lib/domain/usecase/login_usecase.dart
@@ -4,9 +4,14 @@ import 'package:dartz/dartz.dart';
 import 'package:minafarid_app_ar/data/network/failure.dart';
 
 import '../model/models.dart';
+import '../repository/repository.dart';
 import 'base_usecase.dart';
 
 class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
+  final Repository _repository;
+
+  LoginUseCase(this._repository);
+
   @override
   Future<Either<Failure, Authentication>>? execute(LoginUseCaseInput input) {
     // TODO: implement execute
