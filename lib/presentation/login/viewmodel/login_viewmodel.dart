9d9fdc5239fa004be72cf632e63910aa6a86d782
saci zakaria <sacideveloppeur@gmail.com>
2022-09-12 20:43:04 +0200

    extends BaseViewModel

--- lib/presentation/login/viewmodel/login_viewmodel.dart
+++ lib/presentation/login/viewmodel/login_viewmodel.dart
@@ -1,3 +1,15 @@
 //67. Lecture 67 - Adding Login Viewmodel
 
-class LoginViewModel {}
+import 'package:minafarid_app_ar/presentation/base/base_view_model.dart';
+
+class LoginViewModel extends BaseViewModel {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+}
