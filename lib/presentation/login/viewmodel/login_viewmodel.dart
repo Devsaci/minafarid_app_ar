7a0d84b80e78cd1b37a2ce23609e3e06865b9a46
saci zakaria <sacideveloppeur@gmail.com>
2022-09-12 21:14:49 +0200

    StreamController<String>.broadcast();

--- lib/presentation/login/viewmodel/login_viewmodel.dart
+++ lib/presentation/login/viewmodel/login_viewmodel.dart
@@ -7,7 +7,7 @@ import 'package:minafarid_app_ar/presentation/base/base_view_model.dart';
 class LoginViewModel extends BaseViewModel
     with LoginViewModelInputs, LoginViewModelOutputs {
   //Lecture 68 - Adding Streams to Login ViewModel #59
-  final StreamController _userNameStreamController = StreamController<String>();
+  final StreamController _userNameStreamController = StreamController<String>.broadcast();
 
   // inputs
   @override
