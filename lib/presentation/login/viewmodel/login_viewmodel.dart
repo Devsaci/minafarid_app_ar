7347825b0bea0d8f5cbc2ba4febd6ae6c71cdd9e
saci zakaria <sacideveloppeur@gmail.com>
2022-09-12 20:57:49 +0200

    with LoginViewModelInputs, LoginViewModelOutputs

--- lib/presentation/login/viewmodel/login_viewmodel.dart
+++ lib/presentation/login/viewmodel/login_viewmodel.dart
@@ -2,7 +2,8 @@
 
 import 'package:minafarid_app_ar/presentation/base/base_view_model.dart';
 
-class LoginViewModel extends BaseViewModel {
+class LoginViewModel extends BaseViewModel
+    with LoginViewModelInputs, LoginViewModelOutputs {
   @override
   void dispose() {
     // TODO: implement dispose
@@ -11,6 +12,40 @@ class LoginViewModel extends BaseViewModel {
   @override
   void start() {
     // TODO: implement start
+  }
+
+  @override
+  // TODO: implement inputPassword
+  Sink get inputPassword => throw UnimplementedError();
+
+  @override
+  // TODO: implement inputUserName
+  Sink get inputUserName => throw UnimplementedError();
+
+  @override
+  login() {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement outIsPasswordValid
+  Stream<bool> get outIsPasswordValid => throw UnimplementedError();
+
+  @override
+  // TODO: implement outIsUserNameValid
+  Stream<bool> get outIsUserNameValid => throw UnimplementedError();
+
+  @override
+  setPassword(String password) {
+    // TODO: implement setPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  setUserName(String userName) {
+    // TODO: implement setUserName
+    throw UnimplementedError();
   }
 }
 
