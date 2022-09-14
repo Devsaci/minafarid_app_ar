c3207451e2f88677a5abb93b01520bace3cc0ee7
saci zakaria <sacideveloppeur@gmail.com>
2022-09-14 20:58:09 +0200

    part 'freezed_data_classes.freezed.dart';

--- lib/presentation/common/freezed_data_classes.dart
+++ lib/presentation/common/freezed_data_classes.dart
@@ -1,4 +1,13 @@
 //Lecture 69 - Adding Data Classes to Login ViewModel #60
-class LoginObject {
-  LoginObject(String s, String t);
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'freezed_data_classes.freezed.dart';
+
+
+// D:\MinaFarid_App\minafarid_app_ar>flutter
+// pub get && flutter pub run build_runner
+// build --delete-conflicting-outputs ing-output
+@freezed
+class LoginObject with _$LoginObject {
+  factory LoginObject(String userName, String password) = _LoginObject;
 }
