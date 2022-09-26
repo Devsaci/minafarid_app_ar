4985de29318c60ae9dca9910e8228d07c215c983
saci zakaria <sacideveloppeur@gmail.com>
2022-09-26 19:36:37 +0200

    SharedPreferences.getInstance();

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,9 +1,13 @@
 //Lecture 79 - Add dependency injection Instances #70
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final instance = GetIt.instance;
 Future<void> initAppModule() async {
   // app module, its a module where we put all generic dependencies
+
+  // shared prefs instance
+  SharedPreferences.getInstance();
 }
 
 Future<void> initLoginModule() async {}
