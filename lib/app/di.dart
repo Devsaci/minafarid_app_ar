13d782dfbcc23b1498ea6c2fe23878e6f771004d
saci zakaria <sacideveloppeur@gmail.com>
2022-09-26 19:12:21 +0200

    Future<void> initAppModule() async

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -2,3 +2,6 @@
 import 'package:get_it/get_it.dart';
 
 final instance = GetIt.instance;
+Future<void> initAppModule() async {
+  // app module, its a module where we put all generic dependencies
+}
