0b62cd08f368140a602da05405bce6020e86fac2
saci zakaria <sacideveloppeur@gmail.com>
2022-09-28 17:53:22 +0200

    instance.registerLazySingleton<NetworkInfo>

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,6 +1,8 @@
 //Lecture 79 - Add dependency injection Instances #7
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:minafarid_app_ar/app/app_prefs.dart';
+import 'package:minafarid_app_ar/data/network/network_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final instance = GetIt.instance;
@@ -14,7 +16,12 @@ Future<void> initAppModule() async {
   instance.registerLazySingleton<SharedPreferences>(() => sharedPrefs);
 
   // app prefs instance
-  instance.registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+  instance
+      .registerLazySingleton<AppPreferences>(() => AppPreferences(instance()));
+
+  // network info
+  instance.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(InternetConnectionChecker()));
 }
 
 Future<void> initLoginModule() async {}
