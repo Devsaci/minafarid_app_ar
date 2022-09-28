059cdede6f20c580c5af1ce52a56741db1ed2651
saci zakaria <sacideveloppeur@gmail.com>
2022-09-28 18:10:33 +0200

    instance.registerLazySingleton<Repository>

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -9,6 +9,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../data/data_source/remote_data_source.dart';
 import '../data/network/app_api.dart';
+import '../data/repository/repository_impl.dart';
+import '../domain/repository/repository.dart';
 
 final instance = GetIt.instance;
 
@@ -38,6 +40,10 @@ Future<void> initAppModule() async {
   // remote data source
   instance.registerLazySingleton<RemoteDataSource>(
       () => RemoteDataSourceImpl(instance<AppServiceClient>()));
+
+  // repository
+  instance.registerLazySingleton<Repository>(
+      () => RepositoryImpl(instance(), instance()));
 }
 
 Future<void> initLoginModule() async {}
