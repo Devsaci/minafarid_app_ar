2fef78ff4db7b0c5c0d788b8e0b65a6117064a33
saci zakaria <sacideveloppeur@gmail.com>
2022-09-28 18:15:18 +0200

    instance.registerFactory<LoginUseCase>

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -11,6 +11,7 @@ import '../data/data_source/remote_data_source.dart';
 import '../data/network/app_api.dart';
 import '../data/repository/repository_impl.dart';
 import '../domain/repository/repository.dart';
+import '../domain/usecase/login_usecase.dart';
 
 final instance = GetIt.instance;
 
@@ -46,4 +47,7 @@ Future<void> initAppModule() async {
       () => RepositoryImpl(instance(), instance()));
 }
 
-Future<void> initLoginModule() async {}
+Future<void> initLoginModule() async {
+  // Lecture 81 - Add Dependency Injection Login Module #72
+  instance.registerFactory<LoginUseCase>(() => LoginUseCase(instance()));
+}
