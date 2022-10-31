fc81612e70f0a3dc4cdca2c434640c646a588b53
saci zakaria <sacideveloppeur@gmail.com>
2022-10-31 18:54:58 +0100

    StateRendererType getStateRendererType();

--- lib/presentation/common/state_renderer/state_renderer_impl.dart
+++ lib/presentation/common/state_renderer/state_renderer_impl.dart
@@ -1,3 +1,7 @@
 // Lecture 94 - Applying State Renderer - Loading State #82
 
-abstract class FlowState {}
+import 'package:minafarid_app_ar/presentation/common/state_renderer/state_renderer.dart';
+
+abstract class FlowState {
+  StateRendererType getStateRendererType();
+}
