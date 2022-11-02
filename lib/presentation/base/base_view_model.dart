8d2cb3a2011e9fd1e2df9ae274d3536d8da0e8cb
saci zakaria <sacideveloppeur@gmail.com>
2022-11-02 12:46:20 +0100

    void dispose()

--- lib/presentation/base/base_view_model.dart
+++ lib/presentation/base/base_view_model.dart
@@ -7,7 +7,13 @@ abstract class BaseViewModel extends BaseViewModelInputs
     with BaseViewModelOutputs {
   // shared variables and function that will be used through any view model.
 //  Lecture 100 - Adding Input Stream Controller Inside Base ViewModel #88
-  final StreamController _inputStreamController = StreamController<FlowState>.broadcast();
+  final StreamController _inputStreamController =
+      StreamController<FlowState>.broadcast();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
 }
 
 abstract class BaseViewModelInputs {
